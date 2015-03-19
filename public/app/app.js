a5a944c7d62eb78ab098905ea6da267ef3c22af1
Jason Jones <jsjones96@gmail.com>
2015-03-19 20:06:01 +0000

    root

---
+++ public/app/app.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+    angular.module('app', []).controller('testCtrl', function ($scope) {
+        $scope.jobs = [{
+            title: 'Sales Person',
+            description: 'you will fight dragons'
+        }, {
+            title: 'Accountant',
+            description: 'you will use the keyboard'
+        }];
+    });
+}());
