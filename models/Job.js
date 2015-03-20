f3812ba6a178aa6abb68947d664168672c0a1058
Jason Jones <jsjones96@gmail.com>
2015-03-20 13:06:03 +0000

    add Jobs model and connect to mongolabs

---
+++ models/Job.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var jobSchema = mongoose.Schema({
+    title: {type: String},
+    description: {type: String}
+});
+
+var Job = mongoose.model('Job', jobSchema);
+
+exports.seedJobs = function () {
+    Job.find({}).exec(function (err, collection) {
+        if (err) {
+            console.log(err);
+        }
+        if (collection.length === 0) {
+            Job.create({title: 'Cook', description: 'You will be making bagels'});
+            Job.create({title: 'Waiter', description: 'You will be putting food on peoples table'});
+            Job.create({title: 'Programmer', description: 'You will be mindlessly typing for hours'});
+            Job.create({title: 'Axe Maker', description: 'We need many axes made... so many...'});
+        }
+    });
+};
