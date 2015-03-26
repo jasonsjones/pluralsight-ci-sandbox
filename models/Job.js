e3f1d0e88aaf044a5f19b09a94e9f52ae3ee48ab
Jason Jones <jsjones96@gmail.com>
2015-03-26 22:19:04 +0000

    complete module 3

--- models/Job.js
+++ models/Job.js
@@ -5,18 +5,4 @@ var jobSchema = mongoose.Schema({
     description: {type: String}
 });
 
-var Job = mongoose.model('Job', jobSchema);
-
-exports.seedJobs = function () {
-    Job.find({}).exec(function (err, collection) {
-        if (err) {
-            console.log(err);
-        }
-        if (collection.length === 0) {
-            Job.create({title: 'Cook', description: 'You will be making bagels'});
-            Job.create({title: 'Waiter', description: 'You will be putting food on peoples table'});
-            Job.create({title: 'Programmer', description: 'You will be mindlessly typing for hours'});
-            Job.create({title: 'Axe Maker', description: 'We need many axes made... so many...'});
-        }
-    });
-};
+exports.module = mongoose.model('Job', jobSchema);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var jobModel = require('./models/Job');
+var jobsData = require('./jobs-data');
 
 var app = express();
 
@@ -11,24 +12,21 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/api/jobs', function (req, res) {
-    mongoose.model('Job').find({}).exec(function (err, jobs) {
-        if (err) {
-            res.send(err);
-        }
-        res.json(jobs);
+    jobsData.findJobs().then(function (collection) {
+        res.send(collection);
     });
 });
+
 app.get('*', function (req, res) {
     res.render('index');
 });
 
 // mongoose.connect('mongodb://localhost/ci-sandbox');
-mongoose.connect('mongodb://jobfinder:jobfinder@ds053698.mongolab.com:53698/jobfinder');
-
-var connection = mongoose.connection;
-connection.once('open', function () {
+jobsData.connectDB('mongodb://jobfinder:jobfinder@ds053698.mongolab.com:53698/jobfinder')
+.then(function () {
     console.log('successfully connected to mongodb...');
-    jobModel.seedJobs();
+    jobsData.seedJobs();
 });
+
 
 app.listen(process.env.PORT, process.env.IP);
